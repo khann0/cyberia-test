2ed4e050a9e9876ff6d076e41e90ba8b90e1a502
Anna Kholodniak <ayne.hasegawa@gmail.com>
2023-11-10 10:36:14 +0700

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [];
+
+const router = createRouter({
+  routes,
+  // eslint-disable-next-line no-undef
+  history: createWebHistory(process.env.BASE_URL),
+});
+
+export default router;
